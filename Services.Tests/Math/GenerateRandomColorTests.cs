517791395eb52aeddeb77ad70ae7ac06d0bf6b51
Andrew Lane <andrew.w.lane@gmail.com>
2016-05-16 15:28:30 -0400

    Add some unit tests for GenerateRandomColor that actually require mocking and the use of Moq

---
+++ Services.Tests/Math/GenerateRandomColorTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Services.Math;
+using Services.Math.Impl;
+using System;
+
+namespace Services.Tests.Math
+{
+    /// <summary>
+    /// Tests for the GenerateRandomColor class
+    /// </summary>
+    [TestFixture]
+    public class GenerateRandomColorTests
+    {
+        [TestCase(0, ConsoleColor.Red)]
+        [TestCase(1, ConsoleColor.Yellow)]
+        [TestCase(2, ConsoleColor.Magenta)]
+        [TestCase(3, ConsoleColor.Blue)]
+        [TestCase(4, ConsoleColor.Cyan)]
+        [TestCase(5, ConsoleColor.DarkGreen)]
+        [TestCase(6, ConsoleColor.White)]
+        [TestCase(7, ConsoleColor.DarkBlue)]
+        public void TestGetRandomColor(int randomNumberReturned, ConsoleColor expectedColor)
+        {
+            var randomNumberMock = new Mock<IGenerateRandomNumbers>(MockBehavior.Strict);
+            randomNumberMock.Setup(mock => mock.GetRandomInt(0, 7)).Returns(randomNumberReturned);
+
+            var objectUnderTest = new GenerateRandomColor(randomNumberMock.Object);
+
+            objectUnderTest.GetRandomColor().Should().Be(expectedColor);
+        }
+
+        [TestCase(-1)]
+        [TestCase(8)]
+        [TestCase(1000)]
+        public void TestGetRandomColorWithBadRandomNumbersGenerated(int badRandomNumber)
+        {
+            var randomNumberMock = new Mock<IGenerateRandomNumbers>(MockBehavior.Strict);
+            randomNumberMock.Setup(mock => mock.GetRandomInt(0, 7)).Returns(badRandomNumber);
+
+            var objectUnderTest = new GenerateRandomColor(randomNumberMock.Object);
+
+            Action actionThatShouldThrowException = () => objectUnderTest.GetRandomColor();
+            actionThatShouldThrowException.ShouldThrow<IndexOutOfRangeException>();
+        }
+    }
+}
