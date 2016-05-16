29da2fa7a7d49c0159bc1e20331f22be050f0b40
Andrew Lane <andrew.w.lane@gmail.com>
2016-05-16 15:13:57 -0400

    Adding some more tests for generating random numbers

--- Services.Tests/Math/GenerateRandomNumbersTests.cs
+++ Services.Tests/Math/GenerateRandomNumbersTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using Services.Math.Impl;
 using System;
+using System.Diagnostics;
 
 namespace Services.Tests.Math
 {
@@ -18,6 +19,61 @@ namespace Services.Tests.Math
 
             Action actionThatShouldThowException = () => objectUnderTest.GetRandomInt(1, 0);
             actionThatShouldThowException.ShouldThrow<ArgumentOutOfRangeException>();
+        }
+
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(5)]
+        [TestCase(5000)]
+        public void TestGetRandomIntWorksWithNoRange(int min)
+        {
+            var objectUnderTest = new GenerateRandomNumbers();
+            objectUnderTest.GetRandomInt(inclusiveMin: min, inclusiveMax: min).Should().Be(min);
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(5)]
+        [TestCase(50)]
+        [TestCase(500)]
+        [TestCase(5000)]
+        public void TestGetRandomIntGetsAllNumbersInRangeEventually(int max)
+        {
+            TimeSpan maxTimeToRun = TimeSpan.FromSeconds(1); // don't let the test churn for more than a second
+            var stopWatch = new Stopwatch();            
+
+            var objectUnderTest = new GenerateRandomNumbers();
+
+            bool[] found = new bool[max+1];
+
+            bool done = false;
+
+            stopWatch.Start();
+            do
+            {
+                var randomInt = objectUnderTest.GetRandomInt(inclusiveMin: 0, inclusiveMax: max);
+                found[randomInt] = true;
+
+                //assume we're done until we're proven otherwise
+                done = true;
+                for (int i = 0; i <= max; i++)
+                {
+                    if (found[i] == false)
+                    {
+                        done = false;
+                        break;
+                    }
+                }
+
+                if (stopWatch.Elapsed > maxTimeToRun)
+                {
+                    throw new Exception("Test took too long to succeed");
+                }
+            } while (done == false);
+
+            //if we get here, we've succeeded
+            stopWatch.Stop();
         }
     }
 }
